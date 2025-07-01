fd2ac43cd16f0625a49f9c2ed9eea44f1653fe3c
Amira Deuraseh <amiradeu.x@gmail.com>
2025-07-01 11:43:05 +0800

    🪙 Coin > clone individual meshes to figure out correct scale & rotation

--- src/Experience/World/Coins.js
+++ src/Experience/World/Coins.js
@@ -1,6 +1,7 @@
 import gsap from 'gsap'
 
 import Experience from '../Experience'
+import { Euler, Mesh, Quaternion } from 'three'
 
 export default class Coins {
     constructor() {
@@ -10,7 +11,7 @@ export default class Coins {
         this.scene = this.experience.scene
         this.debug = this.experience.debug
 
-        this.counts = 100
+        this.count = 1
         this.items = {}
 
         this.terrain = this.experience.world.terrain
@@ -20,24 +21,97 @@ export default class Coins {
     }
 
     setCoin() {
-        const scale = 0.5
+        const _scale = 100
+
         this.coin = this.resources.items.coinModel.scene
         this.scene.add(this.coin)
 
         this.coin.position.set(0, 1, 3)
-        this.coin.scale.set(scale, scale, scale)
+        // this.coin.scale.set(_scale, _scale, _scale)
 
-        this.coin.traverse((child) => (this.items[child.name] = child))
+        this.coin.traverse((child) => {
+            console.log(
+                child.name,
+                child.type,
+                child.scale,
+                child.rotation,
+                child.position
+            )
+            this.items[child.name] = child
+        })
 
         // console.log(this.items.Coin_1.material)
 
         // rotate the record disk
-        gsap.to(this.coin.rotation, {
-            y: -Math.PI * 2,
-            duration: 8,
-            ease: 'none',
-            repeat: -1,
-        })
+        // gsap.to(this.coin.rotation, {
+        //     y: -Math.PI * 2,
+        //     duration: 8,
+        //     ease: 'none',
+        //     repeat: -1,
+        // })
+
+        const _geometry1 = this.items.Coin_1.geometry.clone()
+        const _geometry2 = this.items.Coin_2.geometry.clone()
+
+        const _material1 = this.items.Coin_1.material
+        const _material2 = this.items.Coin_2.material
+
+        const _mesh1 = new Mesh(_geometry1, _material1)
+        const _mesh2 = new Mesh(_geometry2, _material2)
+
+        const options = {
+            x: Math.PI * 0.5,
+            y: 0,
+            z: 0,
+        }
+        const _euler = new Euler(options.x, options.y, options.z)
+        const _quaternion = new Quaternion()
+        _quaternion.setFromEuler(_euler)
+
+        _mesh1.setRotationFromQuaternion(_quaternion)
+        _mesh2.setRotationFromQuaternion(_quaternion)
+        _mesh1.scale.set(_scale, _scale, _scale)
+        _mesh2.scale.set(_scale, _scale, _scale)
+        _mesh1.position.set(0, 1, 4)
+        _mesh2.position.set(0, 1, 4)
+
+        this.scene.add(_mesh1)
+        this.scene.add(_mesh2)
+
+        if (this.debug.active) {
+            this.debug.ui
+                .add(options, 'x', -Math.PI, Math.PI, 0.01)
+                .onChange(() => {
+                    const _euler = new Euler(options.x, options.y, options.z)
+                    const _quaternion = new Quaternion()
+                    _quaternion.setFromEuler(_euler)
+
+                    _mesh1.setRotationFromQuaternion(_quaternion)
+                    _mesh2.setRotationFromQuaternion(_quaternion)
+                })
+
+            this.debug.ui
+                .add(options, 'y', -Math.PI, Math.PI, 0.01)
+                .onChange(() => {
+                    const _euler = new Euler(options.x, options.y, options.z)
+                    const _quaternion = new Quaternion()
+                    _quaternion.setFromEuler(_euler)
+
+                    _mesh1.setRotationFromQuaternion(_quaternion)
+                    _mesh2.setRotationFromQuaternion(_quaternion)
+                })
+
+            this.debug.ui
+                .add(options, 'z', -Math.PI, Math.PI, 0.01)
+                .onChange(() => {
+                    const _euler = new Euler(options.x, options.y, options.z)
+                    const _quaternion = new Quaternion()
+                    _quaternion.setFromEuler(_euler)
+
+                    _mesh1.setRotationFromQuaternion(_quaternion)
+                    _mesh2.setRotationFromQuaternion(_quaternion)
+                })
+        }
     }
 
     update() {}
@@ -46,21 +120,5 @@ export default class Coins {
         if (!this.debug.active) return
 
         this.debugFolder = this.debug.ui.addFolder('🪙🌕 Coins')
-
-        this.debugFolder.add(
-            this.items.Coin_1.material,
-            'roughness',
-            0,
-            1,
-            0.01
-        )
-
-        this.debugFolder.add(
-            this.items.Coin_1.material,
-            'metalness',
-            0,
-            1,
-            0.01
-        )
     }
 }

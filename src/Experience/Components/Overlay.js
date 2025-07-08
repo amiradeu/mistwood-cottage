192b3a8a9a709e4ddbab91ff5c812b221f021e2c
Amira Deuraseh <amiradeu.x@gmail.com>
2025-07-08 23:38:42 +0800

    🎨 Intro > copywriting and simple animation

--- src/Experience/Components/Overlay.js
+++ src/Experience/Components/Overlay.js
@@ -1,5 +1,4 @@
 import { gsap } from 'gsap'
-import SplitType from 'split-type'
 
 import EventEmitter from '../Utils/EventEmitter.js'
 import Experience from '../Experience.js'
@@ -10,80 +9,64 @@ export default class Overlay extends EventEmitter {
 
         this.experience = new Experience()
         this.resources = this.experience.resources
-        this.sizes = this.experience.sizes
 
         // Setup
         this.setInstance()
         this.setEventListeners()
 
         // State
-        this.begin()
+        this.animateStart()
         this.resources.on('loading', () => this.update())
-        this.resources.on('ready', () => this.ready())
-        this.sizes.on('resize', () => {
-            this.resize()
-        })
+        this.resources.on('ready', () => this.animateReady())
     }
 
     setInstance() {
         // HTML Elements
-        this.overlay = document.querySelector('.overlay')
+        this.overlay = document.querySelector('.intro-overlay')
 
-        this.headings = document.querySelector('.headings')
-        this.intro = document.querySelector('.intro-split')
+        this.headings = this.overlay.querySelectorAll('.headings > *')
 
-        this.loadingText = document.querySelector('.loading-text')
-        this.progressBar = document.querySelector('.loading-progress')
-        this.enterButton = document.querySelector('.enter-button')
+        this.introWrapper = this.overlay.querySelector('.intro-wrapper')
+        this.intros = this.overlay.querySelectorAll('.intro-wrapper > *')
 
-        this.introWrapper = document.querySelector('.intro-wrapper')
-        this.previousContainerWidth = Math.floor(this.introWrapper.clientWidth)
+        this.loadings = this.overlay.querySelectorAll('.loading > *')
+        this.progressBar = this.overlay.querySelector('.loading-progress')
+        this.enterButton = this.overlay.querySelector('.enter-button')
     }
 
     setEventListeners() {
         this.enterButton.addEventListener('click', () => {
-            this.click()
+            this.animateEnd()
             this.trigger('enter')
         })
     }
 
-    begin() {
-        // Animation
-        this.splitText = new SplitType(this.intro)
-        this.text = this.splitText.chars
-
-        this.tlBegin = gsap.timeline()
+    animateStart() {
+        const tween = gsap.timeline()
 
         // loaders
-        this.tlBegin.from([this.loadingText, this.progressBar], {
+        tween.from(this.loadings, {
             y: 50,
             opacity: 0,
             ease: 'power4.inOut',
         })
 
         // headings
-        this.tlBegin.fromTo(
-            this.headings,
-            {
-                opacity: 0,
-                willChange: 'filter, transform',
-                filter: 'blur(12px)',
-            },
-            {
-                ease: 'power2.inOut',
-                opacity: 1,
-                filter: 'blur(0px)',
-                duration: 2,
-            }
-        )
-
-        // intro
-        this.animateIntro()
+        tween.from([...this.headings, ...this.intros], {
+            opacity: 0,
+            y: 30,
+            willChange: 'filter, transform',
+            filter: 'blur(12px)',
+            ease: 'power2.out',
+            stagger: 0.2,
+            duration: 4,
+        })
     }
 
-    ready() {
+    animateReady() {
         let tween = gsap.timeline()
-        tween.to([this.loadingText, this.progressBar], {
+
+        tween.to(this.loadings, {
             y: 50,
             opacity: 0,
             willChange: 'filter, transform',
@@ -92,6 +75,7 @@ export default class Overlay extends EventEmitter {
             display: 'none',
             ease: 'power4.inOut',
         })
+
         tween.fromTo(
             this.enterButton,
             {
@@ -115,86 +99,17 @@ export default class Overlay extends EventEmitter {
         this.progressBar.innerHTML = `${progress}%`
     }
 
-    click() {
-        // console.log('enter click')
-        this.tlEnd = gsap.timeline()
+    animateEnd() {
+        // console.log('enter experience')
+        let tween = gsap.timeline()
 
-        if (this.introTween && this.introTween.isActive()) {
-            this.introTween.invalidate().kill()
-            // this.tlBegin.progress(1.0)
-
-            this.tlEnd.to(this.text, {
-                ease: 'sine',
-                opacity: 1,
-                skewX: 0,
-                filter: 'blur(0px)',
-                duration: 3,
-                onComplete: () => {
-                    this.splitText.revert()
-                },
-            })
-        }
-
-        this.tlEnd.to(
-            this.overlay,
-            {
-                visibility: 'hidden',
-                borderRadius: '100%',
+        tween.to(this.overlay, {
+            opacity: 0,
+            duration: 2,
+            ease: 'power2.out',
+            onComplete: () => {
+                this.overlay.style.visibility = 'hidden'
             },
-            '<'
-        )
-    }
-
-    // re-split text if text container width changes
-    resize() {
-        const width = Math.floor(this.introWrapper.clientWidth)
-
-        if (
-            this.previousContainerWidth &&
-            this.previousContainerWidth !== width &&
-            this.introTween.progress() !== 1 // not yet ended
-        ) {
-            this.previousContainerWidth = width
-            // console.log('progress', this.introTween.progress())
-            // console.log('splitting')
-
-            this.splitText.split()
-            this.text = this.splitText.chars
-
-            this.lastIntroTweenTime = this.introTween.totalTime()
-            // console.log('lastTime', this.lastIntroTweenTime)
-
-            this.introTween.kill()
-            this.animateIntro()
-        }
-    }
-
-    animateIntro() {
-        this.introTween = gsap.timeline()
-        this.introTween.fromTo(
-            this.text,
-            {
-                opacity: 0,
-                skewX: -20,
-                willChange: 'filter, transform',
-                filter: 'blur(8px)',
-            },
-            {
-                ease: 'sine',
-                opacity: 1,
-                skewX: 0,
-                filter: 'blur(0px)',
-                stagger: 0.04,
-                duration: 2,
-                onComplete: () => {
-                    this.splitText.revert()
-                },
-            },
-            '<1'
-        )
-        // continue animation at time before resize
-        if (this.lastIntroTweenTime) {
-            this.introTween.totalTime(this.lastIntroTweenTime)
-        }
+        })
     }
 }

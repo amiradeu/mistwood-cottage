d08e7f180d37256353d5fd373480bb1876590884
Amira Deuraseh <amiradeu.x@gmail.com>
2025-07-09 21:33:17 +0800

    📊 Vercel Insights

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import Experience from './Experience/Experience.js'
 import { injectSpeedInsights } from '@vercel/speed-insights'
+import Experience from './Experience/Experience.js'
 
-const experience = new Experience(document.querySelector('canvas.webgl'))
 injectSpeedInsights()
+const experience = new Experience(document.querySelector('canvas.webgl'))

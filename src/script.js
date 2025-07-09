674941a62afe6361686148d88bb76c62a4ead126
Amira Deuraseh <amiradeu.x@gmail.com>
2025-07-09 21:56:57 +0800

    🧹 Cleanup

--- src/script.js
+++ src/script.js
@@ -1,7 +1,5 @@
 import { injectSpeedInsights } from '@vercel/speed-insights'
 import Experience from './Experience/Experience.js'
 
-injectSpeedInsights({
-    route: '/',
-})
+injectSpeedInsights()
 const experience = new Experience(document.querySelector('canvas.webgl'))

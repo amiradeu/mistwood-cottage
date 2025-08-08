ba4606b5b9097e5bc68ea129d98ab0ee4544b32e
Amira Deuraseh <amiradeu.x@gmail.com>
2025-08-08 16:13:45 +0800

    Vercel > add analytics

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 import { injectSpeedInsights } from '@vercel/speed-insights'
+import { inject } from '@vercel/analytics'
+
 import Experience from './Experience/Experience.js'
 
 injectSpeedInsights()
 const experience = new Experience(document.querySelector('canvas.webgl'))
+inject({
+    mode: 'production',
+})
